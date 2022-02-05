1353816cd8b8e15977a765943d1fe0e742cc8d4b
Viktor Liljenberg <viktorlilienberg@gmail.com>
2022-02-05 22:21:29 +0100

    all products

--- my-app/src/Product.js
+++ my-app/src/Product.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import './Product.css'
 import './Home.css'
-import prodGlock from './img/Product_Glock.jpg'
 
-function Product()
+function Product({title, image, price, rating}) //prop
 {
     return(
         <div className='product'>
 <div className='product_info'>
-    <p className='product_name'>The lean startup</p>
+    <p>{title}</p>
     <p className='product_price'> 
     <small>$</small>
-    <strong>19.99</strong>
+    <strong>{price}</strong>
     </p>
    <div className='product_rating'>
-        <p>⭐</p>
-        <p>⭐</p>
-        <p>⭐</p>
+        {Array(rating).fill().map((_, i) => (
+                    <p>⭐</p>
+        ))}
    </div>
 </div>
 
-<img src={prodGlock} alt="{prodGlock}" />
+<img src={image}  />
 <button>Add to basket</button>
 </div>
     );
