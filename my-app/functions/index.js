d7c9e97ce5a974d5102b13669198732c27fe85dc
Viktor Liljenberg <viktorlilienberg@gmail.com>
2022-02-06 22:36:44 +0100

    added stripe

---
+++ my-app/functions/index.js
@@ -0,0 +1,15 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")('sk_test_51KQGFgAqBpxcVG5VhlrqTiRbRgik8G2f8tioHrmSOf4xff8NAt8T5eU1U5J2wXkf8aGViTEbPlVhBi19eixNfd3S009p9zRmbj');
+
+//-API
+
+//-App config
+
+
+//-Middlewares
+
+//-API routes
+
+//-Listen command
