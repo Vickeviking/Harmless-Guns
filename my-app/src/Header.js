5997b031a9bb66c021d56e422b9e24f2e8a09925
Viktor Liljenberg <viktorlilienberg@gmail.com>
2022-02-07 22:40:40 +0100

    gjort nav bar + maincontent responsive

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dashboard_Css/Header.css";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faSearch} from '@fortawesome/free-solid-svg-icons'
@@ -18,87 +18,57 @@ function Header() {
       auth.signOut();
     }
   }
-
+   const [isActive, setActive] = useState("false");
+   const handleToggle = () => {
+    setActive(!isActive);
+   }
   return (
-    <div className="FullHeader"> 
-<div className="Header">
-  <Link to="/">
-  <h1 className="rubrikName">Harmless Guns</h1>
-  </Link>
-   <div className="header_search">
-        <input
-        className="header_searchInput" type="text"/>
-         <FontAwesomeIcon className="header_searchIcon" icon={faSearch}></FontAwesomeIcon>
-    
- </div>
+    <nav>
+      <div className="container">
 
-    <div className="header_nav">
+      <Link to={'/'} style={{ textDecoration: 'none' }}>
+      <h1>Harmless Guns</h1>
+      </Link>
+     
 
-        <div className="lang">
-        <FontAwesomeIcon className="header_globeIcon" icon={faGlobe}></FontAwesomeIcon>
-        <FontAwesomeIcon className="header_globeIcon" icon={faCaretDown}></FontAwesomeIcon>
-        </div>
+        <div className={isActive ? "menu" : "dropbar"}>
+         
+          <Link to={'/'}>
+          <h2>Home</h2>
+          </Link>
 
-        <Link to={!user && '/login'}>
-        <div onClick={handleAuthentication} className="header_option">
-        <span className="header_optionLineOne">Hello {user ? user?.email : 'Guest'}</span>
-        <span className="header_optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span>
-        </div>
-        </Link>
+          <Link to={'/login'}>
+          <h2>Our Stock</h2>
+          </Link>
 
+          <Link to={'/login'}>
+          <h2>{user ? 'Sign Out' : 'Sign In'}</h2>
+          </Link>
+
+          
         <Link to={'/orders'}>
         <div className="header_option">
-        <span className="header_optionLineOne">Return</span>
-        <span className="header_optionLineTwo">& Orders</span>
+        <span className="header_optionLineOne">Return & Orders</span>
         </div>
         </Link>
 
-        <Link to="/checkout">
+          <Link to="/checkout">
         <div className="header_optionBasket">
             <FontAwesomeIcon className="header_shopingBasket" icon={faShoppingCart}></FontAwesomeIcon>
             <span className="header_optionLineTwo header_basketCount">{basket?.length}</span>
         </div>
         </Link>
-        
 
-    </div>
+        </div>
 
-</div>
-
-
-    
-<div className='SubHeaderNav'>
-
-<div className='More'>
-<FontAwesomeIcon className='BarsIcon' icon={faBars}></FontAwesomeIcon>
-<h2>All</h2>
-</div>
-
-<div className='SubNav'>
-    
-<div className='PrimeNav'> 
-<h2>Collections </h2>
-<FontAwesomeIcon className='CaretIcon' icon={faCaretDown}></FontAwesomeIcon>
-</div>
-<h2><a href="">Best Seller</a> </h2>
-<h2><a href="">Offers</a> </h2>
-<h2><a href="">Gift Idea</a> </h2>
-<h2><a href="">Customer Service</a> </h2>
-<h2><a href="">Valentines Guns</a> </h2>
-
-</div>
-
-<div className='Announcement'>
-  <div className='Space'></div>
-<h1>Discover our new 1945 Collection  </h1>
-</div>
-
-    </div>
-  
-
-
-    </div>
-
+        <button onClick={handleToggle} className="hamburger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+     
+    </nav>
   );
 
   
