5802a965f4cf8745b2af18dbd0d44fca37e5ef6c
Vickeviking <92083048+Vickeviking@users.noreply.github.com>
2022-02-10 12:16:32 +0100

    footer börjar formas

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -4,7 +4,34 @@ import './Dashboard_Css/Footer.css'
 function Footer() {
   return (
       <footer>
-          
+
+        <div className='Del'>
+          <h1> Hej jag är en footer </h1>
+          <h1> Hej jag är en footer </h1>
+          <h1> Hej jag är en footer </h1>
+          <h1> Hej jag är en footer </h1>
+          <h1> Hej jag är en footer </h1> 
+        </div>
+
+        
+
+        <div className='Del'>
+          <h1> Hej jag är en footer </h1>
+          <h1> Hej jag är en footer </h1>
+          <h1> Hej jag är en footer </h1>
+          <h1> Hej jag är en footer </h1>
+          <h1> Hej jag är en footer </h1> 
+        </div>
+
+        
+
+        <div className='Del'>
+          <h1> Hej jag är en footer </h1>
+          <h1> Hej jag är en footer </h1>
+          <h1> Hej jag är en footer </h1>
+          <h1> Hej jag är en footer </h1>
+          <h1> Hej jag är en footer </h1> 
+        </div>
       </footer>
   );
 }
