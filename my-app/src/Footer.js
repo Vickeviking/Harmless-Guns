cf558812f069aac61c5f9f71a03b332d456c4331
Vickeviking <92083048+Vickeviking@users.noreply.github.com>
2022-02-10 12:52:15 +0100

    footer

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -4,34 +4,48 @@ import './Dashboard_Css/Footer.css'
 function Footer() {
   return (
       <footer>
-
+        <div className='omslag'>
         <div className='Del'>
           <h1> Hej jag är en footer </h1>
-          <h1> Hej jag är en footer </h1>
-          <h1> Hej jag är en footer </h1>
-          <h1> Hej jag är en footer </h1>
-          <h1> Hej jag är en footer </h1> 
+          <h2> Hej jag är en footer </h2>
+          <h2> Hej jag är en footer </h2>
+          <h2> Hej jag är en footer </h2>
+          <h2> Hej jag är en footer </h2> 
         </div>
 
         
 
         <div className='Del'>
           <h1> Hej jag är en footer </h1>
-          <h1> Hej jag är en footer </h1>
-          <h1> Hej jag är en footer </h1>
-          <h1> Hej jag är en footer </h1>
-          <h1> Hej jag är en footer </h1> 
+          <h2> Hej jag är en footer </h2>
+          <h2> Hej jag är en footer </h2>
+          <h2> Hej jag är en footer </h2>
+          <h2> Hej jag är en footer </h2> 
         </div>
 
         
 
         <div className='Del'>
           <h1> Hej jag är en footer </h1>
-          <h1> Hej jag är en footer </h1>
-          <h1> Hej jag är en footer </h1>
-          <h1> Hej jag är en footer </h1>
-          <h1> Hej jag är en footer </h1> 
+          <h2> Hej jag är en footer </h2>
+          <h2> Hej jag är en footer </h2>
+          <h2> Hej jag är en footer </h2>
+          <h2> Hej jag är en footer </h2> 
         </div>
+        </div>
+
+        <div className='borderStyling'>
+          
+        </div>
+
+        <div className='Partners'>
+<div className='Partner_item'></div>
+<div className='Partner_item'></div>
+<div className='Partner_item'></div>
+<div className='Partner_item'></div>
+<div className='Partner_item'></div>
+        </div>
+
       </footer>
   );
 }
