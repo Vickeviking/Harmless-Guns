b653d4c8176fa1e3568c8007ab3d6b26e1a3ee41
Vickeviking <92083048+Vickeviking@users.noreply.github.com>
2022-02-11 16:02:24 +0100

    footer pics

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -1,52 +1,76 @@
-import React from 'react';
-import './Dashboard_Css/Footer.css'
+import React from "react";
+import "./Dashboard_Css/Footer.css";
+import partner from "./img/partner.gif";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import instagram from "./img/instagram.gif";
+import facebook from "./img/facebook.gif";
+import linkedIn from "./img/linkedIn.gif";
 
 function Footer() {
   return (
-      <footer>
-        <div className='omslag'>
-        <div className='Del'>
+    <footer>
+      <div className="omslag">
+        <div className="Del">
           <h1> Hej jag är en footer </h1>
           <h2> Hej jag är en footer </h2>
           <h2> Hej jag är en footer </h2>
           <h2> Hej jag är en footer </h2>
-          <h2> Hej jag är en footer </h2> 
+          <h2> Hej jag är en footer </h2>
         </div>
 
-        
-
-        <div className='Del'>
+        <div className="Del">
           <h1> Hej jag är en footer </h1>
           <h2> Hej jag är en footer </h2>
           <h2> Hej jag är en footer </h2>
           <h2> Hej jag är en footer </h2>
-          <h2> Hej jag är en footer </h2> 
+          <h2> Hej jag är en footer </h2>
         </div>
 
-        
-
-        <div className='Del'>
+        <div className="Del">
           <h1> Hej jag är en footer </h1>
           <h2> Hej jag är en footer </h2>
           <h2> Hej jag är en footer </h2>
           <h2> Hej jag är en footer </h2>
-          <h2> Hej jag är en footer </h2> 
+          <h2> Hej jag är en footer </h2>
         </div>
-        </div>
+      </div>
 
-        <div className='borderStyling'>
-          
-        </div>
+      <div className="borderStyling"></div>
 
-        <div className='Partners'>
-<div className='Partner_item'></div>
-<div className='Partner_item'></div>
-<div className='Partner_item'></div>
-<div className='Partner_item'></div>
-<div className='Partner_item'></div>
+      <div className="Partners">
+        <div className="Partner_item">
+          {" "}
+          <img src={partner} />{" "}
         </div>
+        <div className="Partner_item">
+          {" "}
+          <img src={partner} />{" "}
+        </div>
+        <div className="Partner_item">
+          {" "}
+          <img src={partner} />{" "}
+        </div>
+        <div className="Partner_item">
+          {" "}
+          <img src={partner} />{" "}
+        </div>
+      </div>
 
-      </footer>
+      <div className="SocialaMedier">
+        <div className="Partner_item">
+          {" "}
+          <img src={instagram} />{" "}
+        </div>
+        <div className="Partner_item">
+          {" "}
+          <img src={facebook} />{" "}
+        </div>
+        <div className="Partner_item">
+          {" "}
+          <img src={linkedIn} />{" "}
+        </div>
+      </div>
+    </footer>
   );
 }
 
